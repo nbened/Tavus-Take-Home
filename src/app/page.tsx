8a36c46c9fee1380212ee2e425dea5f064b8e900
Nic Benedetto <nickbenedetto11@gmail.com>
2026-03-25 21:31:08 -0700

    memeber

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
         <div />
         <div className="flex flex-col items-center gap-6">
           <h1 className="text-3xl font-semibold tracking-tight max-w-lg">
-            You&apos;ve just been hired on as the newest engineer of{" "}
+            You&apos;ve just been hired on as the newest member of{" "}
             <span className="text-blue-600">Joja Mart</span>
           </h1>
           <p className="text-neutral-500 text-sm max-w-sm">
